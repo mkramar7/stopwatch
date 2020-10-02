b75793efa383fa4f9948790280eecdc3e66dc7b6
Marko Kramar <marko.kramar7@gmail.com>
2020-10-02 10:07:51 +0200

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Marko Kramar on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
